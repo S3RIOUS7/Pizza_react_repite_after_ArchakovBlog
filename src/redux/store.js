c026d1be5409e0c4ea02b79366184285aa584d6e
S3RIOUS7 <107929484+S3RIOUS7@users.noreply.github.com>
2023-01-29 09:39:04 +0200

    last commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import filterSlice from './slices/filterSlice'
+import filterReducer from './slices/filterSlice'
 
 export const store = configureStore({
   reducer: { filter: filterReducer },
