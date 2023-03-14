814f04594f4541bb0f6347855696845bd1f7fa4e
S3RIOUS7 <107929484+S3RIOUS7@users.noreply.github.com>
2023-03-14 14:30:35 +0200

    last commit

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-
-function Categories( { value, onChangeCategory } ){
- 
-
+const Categories = ({ value, onChangeCategory }) => { 
   const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые'];
 
+ 
     return(
       <div className="categories">
       <ul>
